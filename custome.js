c9cdb8dd358b0533b4a3bdaa9d1e5ab7a52958c5
vanhellsing102 <mdm78774@gmail.com>
2024-03-11 18:27:15 +0600

    tea house

---
+++ custome.js
@@ -0,0 +1,12 @@
+<script>
+    tailwind.config = {
+      theme: {
+        extend: {
+          colors: {
+            clifford: '#da373d',
+            "common-color": #F1D710;
+          }
+        }
+      }
+    }
+  </script>
